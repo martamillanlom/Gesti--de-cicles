ed9f210ca1db5de52f9843c3d13a19ef8acbef0d
Marta Millan Lombraña <marta.millan@iesjoandaustria.org>
2022-10-26 16:23:13 +0200

    Gestió de cicles

---
+++ script.js
@@ -0,0 +1,106 @@
+let llistatCicles = [];
+
+function afegirCicle(){
+    let nom = document.getElementById("cicle_nom").value;
+    let categoria = document.getElementById("cicle_categoria").value;
+    let numAlumnes = document.getElementById("cicle_alumnes").value;
+    let abreviatura = document.getElementById("cicle_abr").value;
+
+    let cicle = {nom: nom, categoria: categoria, numAlumnes: numAlumnes, abreviatura: abreviatura}
+    console.log(cicle);
+
+    if(document.getElementById("editCicle").value === "-1"){
+        //Afegim el cicle al llistat
+        llistatCicles.push(cicle);
+    }else{
+        //Editar cicle
+
+    }
+    
+    //Actualitzem el selector
+    actualitzarSelector();
+
+    //Printem la llista
+    printLlistat(llistatCicles);
+
+    //Netegem els formularis
+    netejarFormularis();
+
+    document.getElementById("editCicle").value=-1;
+}
+
+function afegirModul(){
+    let cicle = document.getElementById("modul_cicle").value;
+    let modul_nom = document.getElementById("modul_nom").value;
+    let modul_num = document.getElementById("modul_num").value;
+    let modul_hores = document.getElementById("modul_hores").value;
+
+    let modul = {cicle: cicle, nom: modul_nom, num: modul_num, hores: modul_hores}
+    console.log(modul);
+
+    //Printem la llista
+    printLlistat(llistatCicles);
+
+    //Netegem els formularis
+    netejarFormularis();
+}
+
+//Funció per llistar els cicles
+function printLlistat (llistat){
+    let str="";
+    llistat.forEach(function(element, index){
+        str += `<div class="block p-6 mb-3 w-full bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${element.abreviatura.toUpperCase()}. ${element.nom}</h5>
+                    <h6 class="text-gray-700 dark:text-gray-400">${element.categoria}</h6>
+                    <p class="font-normal text-gray-700 dark:text-gray-400 mb-5">Num d'alumnes: ${element.numAlumnes}</p>
+
+                    <button type="button" onClick="removeCicle(${index})" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Eliminar</button>
+                    <button type="button" onClick="editCicle(${index})" class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Editar</button>
+                    <button type="button" onClick="calculHores(${index})" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Càlcul hores</button>
+
+
+                </div>`;
+    });
+
+    document.getElementById("llistat").innerHTML=str;
+}
+
+//Funció per actualitzar el selector de cicles cada vegada que afegim un cicle
+function actualitzarSelector(){
+    let select = document.getElementById('modul_cicle');
+
+    llistatCicles.forEach(function(element, index){
+        let opt = document.createElement('option');
+        opt.value = index;
+        opt.text = element.nom;
+        select.appendChild(opt);
+    });
+}
+
+//Funció per eliminar un cicle
+function removeCicle(i){
+
+}
+
+//Funció per editar un cicle
+function editCicle(i){
+    document.getElementById("cicle_nom").value = llistatCicles[i].nom;
+    document.getElementById("cicle_categoria").value = llistatCicles[i].categoria;
+    document.getElementById("cicle_alumnes").value = llistatCicles[i].numAlumnes;
+    document.getElementById("cicle_abr").value = llistatCicles[i].abreviatura;
+
+    document.getElementById("editCicle").value=i;
+}
+
+//Funció per netejar els formularis
+function netejarFormularis(){
+    var inputs = document.getElementsByTagName("input");
+    for (let i=0; i < inputs.length; i++) {
+        inputs[i].value = "";
+    }
+
+    var selects = document.getElementsByTagName("select");
+    for (let i=0; i < selects.length; i++) {
+        selects[i].value = 0;
+    }
+}
